9b6f9913b5503d4e932e96820884004b2c77ac89
Joan Albert Segura Rueda <jalbertsr@protonmail.ch>
2020-07-28 15:35:30 +0200

    set up project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'error',
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+};
