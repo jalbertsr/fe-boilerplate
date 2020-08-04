45bd28b496c6e25ac585362922a119b4d1148872
Joan Albert Segura <jalbertsr@protonmail.ch>
2020-08-04 20:21:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import css from 'styled-jsx/css';
 
-const App = () => <h1>FE Boilerplate</h1>;
+const style = css`
+  h1 {
+    color: red;
+  }
+`;
+
+const App = () => (
+  <>
+    <h1>FE Boilerplate</h1>
+    <style jsx>{style}</style>
+  </>;
 
 export default App;
