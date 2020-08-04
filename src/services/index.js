d0e0b5ba663cc4e1e33f6e622bad247575cccaff
Joan Albert Segura <jalbertsr@protonmail.ch>
2020-08-04 20:19:00 +0200

    Delete index.js

--- src/services/index.js
+++
@@ -1,9 +0,0 @@
-export const getCards = () => JSON.parse(localStorage.getItem('cardsList'));
-
-export const addCard = (newCard) => {
-  if (getCards()) {
-    localStorage.setItem('cardsList', JSON.stringify([...getCards(), newCard]));
-  } else {
-    localStorage.setItem('cardsList', JSON.stringify([newCard]));
-  }
-};
